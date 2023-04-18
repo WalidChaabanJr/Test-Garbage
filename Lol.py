5e264ea8d0162912bbf66c710ae4593d9e028b68
WalidChaabanJr <131113035+WalidChaabanJr@users.noreply.github.com>
2023-04-17 23:14:45 -0400

    Add files via upload

---
+++ Lol.py
@@ -0,0 +1,7 @@
+
+num = 6
+r = str("x")
+while num >= 0:
+    print(r)
+    r ="x"+ r
+    num = num - 1
---
+++ main.py
@@ -0,0 +1,5 @@
+number = int(input("How many Lines do you want?"))
+Text = input("What text do you want?")
+while int(number) >= 1:
+    print(Text)
+    number = number-1
